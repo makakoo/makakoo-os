f56ea367f0e615580d13dcfc13709f99440f20cb
rschumann <sebastian.schkudlara@gmail.com>
2026-06-14 15:11:12 +0200

    chore(release): finalize Homebrew formula for v0.1.29

--- distribution/homebrew/makakoo.rb
+++ distribution/homebrew/makakoo.rb
@@ -20,22 +20,22 @@ class Makakoo < Formula
   on_macos do
     on_arm do
       url "https://github.com/makakoo/makakoo-os/releases/download/v#{version}/makakoo-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_AT_RELEASE_MACOS_ARM64"
+      sha256 "b554ad3a162295ccfc0523be7fcf0c67be7c12745dfc4c4ed88ff939a776509c"
     end
     on_intel do
       url "https://github.com/makakoo/makakoo-os/releases/download/v#{version}/makakoo-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_AT_RELEASE_MACOS_X86_64"
+      sha256 "2c4abed4d2ddb14ed4294688d92000adff34f7a32e74be6e983b64e4c92796ee"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/makakoo/makakoo-os/releases/download/v#{version}/makakoo-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_AT_RELEASE_LINUX_X86_64"
+      sha256 "a6bc6742a4be1bd1249417076d82db24f1405bd31d1511e8f9ca0427285b963e"
     end
     on_arm do
       url "https://github.com/makakoo/makakoo-os/releases/download/v#{version}/makakoo-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_AT_RELEASE_LINUX_ARM64"
+      sha256 "283abedbd0ec9a064b1f570262fbb8e68fa43f0dfbe77b1c21295a477cf1f32f"
     end
   end
 
